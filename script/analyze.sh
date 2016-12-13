f43d67a88882db3a1225c5f4d50a5f213cbf6641
reinvantveer <reinvantveer@gmail.com>
2016-12-13 21:36:52 +0100

    Mess around with scripts for ease

---
+++ script/analyze.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+node ld-transformer/bin/analyze.js -i ~/surfdrive/DANS/target/csv/ -e .csv
--- script/mif2csv.sh
+++ script/mif2csv.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -ex
 while IFS= read -r -d '' file
 do
   infile=$(printf '%q\n' "${file}")
-  outfile=$(printf '%q\n' "$(dirname ${file})"/"$(basename ${file} .mif)").csv
-  ogr2ogr -f CSV ${outfile} ${infile} -lco GEOMETRY=AS_WKT -lco CREATE_CSVT=YES -lco SEPARATOR=TAB
+  outfile=$(printf '%q\n' /home/vagrant/surfdrive/DANS/target/csv/"$(basename ${file})").csv
+  ogr2ogr -f "CSV" ${outfile} ${infile} -overwrite -lco GEOMETRY=AS_WKT -lco CREATE_CSVT=YES -lco SEPARATOR=COMMA
 done < <(find "$1" -name '*.mif' -print0)
