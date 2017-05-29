197683dccf48c7c940ae89eae824b070279bc048
Rein van t Veer <r.h.vant.veer@vu.nl>
2017-05-29 09:36:05 +0200

    Allow for message

--- etl/lib/ProgressBar.py
+++ etl/lib/ProgressBar.py
@@ -9,16 +9,17 @@ This module provides a simple console progress bar indicator
 class ProgressBar:
     @staticmethod
     # TODO: refactor to allow creation of a singleton progress_bar that has a default bar length
-    def update_progress(progress, bar_length=10):
+    def update_progress(progress, message="", bar_length=10):
         """
         update_progress() : Displays or updates a console progress bar
 
         The method simply repeats  on the console each time the method is called
-        :param bar_length: Modify this to change the length of the progress bar
         :param progress: Accepts a float between 0 and 1. Any int will be converted to a float.
         A value under 0 represents a 'halt'.
         A value at 1 or bigger represents 100%
-        :return: 
+        ;:param message: an optional message to include in the update
+        :param bar_length: Modify this to change the length of the progress bar
+        :return:
         """
         status = ""
         if isinstance(progress, int):
@@ -34,6 +35,6 @@ class ProgressBar:
             status = "Done...\r\n"
         block = int(round(bar_length*progress))
         progress_rounded = "{:10.2f}".format(float(progress*100))
-        text = "\rPercent: [{0}] {1}% {2}".format("#"*block + "-"*(bar_length-block), progress_rounded, status)
+        text = "\rPercent: [{0}] {1}% {2} - {3}".format("#"*block + "-"*(bar_length-block), progress_rounded, status, message)
         sys.stdout.write(text)
         sys.stdout.flush()
